927b7bee865277bb61102193aef43f7fc5dbe13b
Ariel Soto <arielsotocaro@gmail.com>
2022-08-17 16:33:37 -0400

    keep this
    copied by gdata package

--- R/keep_this.R
+++ R/keep_this.R
@@ -3,11 +3,28 @@
 ##' input the elements as: "a","b",...
 ##'
 ##' @export
+##'
 
-keep_this <- function(elements){
 
-  rm(list=ls()[! ls() %in% c(elements)])
-
+keep_this <- function (..., list = character(0), all = FALSE, sure = FALSE)
+{
+  if (missing(...) && missing(list)) {
+    warning("keep something, or use rm(list=ls()) to clear workspace - ",
+            "nothing was removed")
+    return(invisible(NULL))
+  }
+  names <- as.character(substitute(list(...)))[-1]
+  list <- c(list, names)
+  keep.elements <- match(list, ls(1, all.names = all))
+  if (any(is.na(keep.elements))) {
+    warning("you tried to keep \"", list[which(is.na(keep.elements))[1]],
+            "\" which doesn't exist in workspace - nothing was removed",
+            sep = "")
+    return(invisible(NULL))
+  }
+  if (sure)
+    rm(list = ls(1, all.names = all)[-keep.elements], pos = 1)
+  else return(ls(1, all.names = all)[-keep.elements])
 }
 
 
---
+++ R/mi_test.R
@@ -0,0 +1,16 @@
+##' Delete everything in the R environment BUT
+##' keep the elements in the function.
+##' input the elements as: "a","b",...
+##'
+##' @export
+##'
+##' @param cosa single object or a list of them to be removed "a","b",...
+##'
+
+mi_test <- function(cosa){
+
+  (cosa+2)^2
+
+}
+
+
